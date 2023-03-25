c83e7a6a9c833dd198576f177904ba6bad4f3a77
Chloe Edwards <git@edken.co.uk>
2023-03-25 19:41:30 +0000

    implemented move command

--- RedirectBot/Program.cs
+++ RedirectBot/Program.cs
@@ -18,7 +18,7 @@ public class Program
 			var config_file = File.ReadAllText("app_config.json");
 			if(config_file == null)
 			{
-				Console.WriteLine("Configuration file \"app_config.json\" not found");
+				program.Log("Configuration file \"app_config.json\" not found");
 				return Task.CompletedTask;
 			}
 
@@ -26,7 +26,7 @@ public class Program
 		}
 		catch (Exception ex)
 		{
-			Console.WriteLine("Unhandled top-level exception in Application: " + ex.Message);
+			Console.WriteLine($"Unhandled top-level exception in Application: {ex.Message}");
 			return Task.CompletedTask;
 		}
 	}
@@ -37,6 +37,8 @@ public class Program
 	{
 		_client = new DiscordSocketClient();
 		_client.Log += Log;
+		_client.Ready += OnClientReady;
+		_client.SlashCommandExecuted += OnMoveCommandExecuted;
 	}
 
 	public async Task RunAsync(AppConfig? config)
@@ -52,18 +54,98 @@ public class Program
 		}
 
 		await _client.LoginAsync(TokenType.Bot, config.token);
-		await _client.StopAsync();
+		await _client.StartAsync();
 
 		//just block for now
 		await Task.Delay(-1);
 	}
 
+	public async Task OnClientReady()
+	{
+		//this doesn't actually need to be done every time it's run, only when the command is new or changing
+
+		var commandBuilder = new SlashCommandBuilder();
+
+
+		commandBuilder.WithName("move")
+			.WithDescription("Send discussion to another channel")
+			.AddOption("target-channel",
+				ApplicationCommandOptionType.Channel,
+				"The channel to send discussion to",
+				isRequired:true,
+				channelTypes:new List<ChannelType> { ChannelType.Text });
+
+		try
+		{
+			await _client.CreateGlobalApplicationCommandAsync(commandBuilder.Build());
+
+			await Log("registered the move slash command");
+		}
+		catch(Exception ex) 
+		{
+			await Log(ex.Message);
+		}
+	}
+
+	public async Task OnMoveCommandExecuted(SocketSlashCommand command)
+	{
+		if(command.CommandName != "move")
+		{
+			//not our job to respond to it
+			return;
+		}
+
+		if(command.Channel.GetChannelType() != ChannelType.Text && command.Channel.GetChannelType() != ChannelType.PublicThread)
+		{
+			await command.RespondAsync("This command can only be used in text channels and public threads", ephemeral: true);
+			return;
+		}
+
+		SocketTextChannel? targetChannel = null;
+		SocketTextChannel originChannel = (SocketTextChannel)command.Channel;
+
+        foreach (var option in command.Data.Options)
+        {
+			if(option.Name == "target-channel")
+			{
+				targetChannel = (SocketTextChannel)option.Value;
+			}
+        }
+		if(targetChannel == null)
+		{
+			
+			await command.RespondAsync("An internal error occurred, contact this bot's maintainer", ephemeral:true);
+			await Log("move command had no target channel offered");
+			return;
+		}
+
+		//log that the command happened
+
+		if (targetChannel.Id == command.Channel.Id)
+		{
+			await command.RespondAsync("You can't move this discussion to a channel it's already in", ephemeral:true);
+			return;
+		}
+
+		await command.RespondAsync("Moving the discussion now...", ephemeral:true);
+
+		var destinationMessage = await targetChannel.SendMessageAsync($"Continuing the discussion from <#{originChannel.Id}>");
+		var originMessage = await originChannel.SendMessageAsync($"This discussion has moved to <#{targetChannel.Id}> (moved by <@{command.User.Id}>)\n" +
+			$"Carry it on at {destinationMessage.GetJumpUrl()}");
+
+		await destinationMessage.ModifyAsync(messageProperties => messageProperties.Content = $"Continuing the discussion from <#{originChannel.Id}>\n" +
+		$"Carried on from {originMessage.GetJumpUrl()}");
+	}
 
 	private Task Log(LogMessage msg)
 	{
-		Console.WriteLine(msg.ToString());
+		return Log(msg.ToString());
+	}
+
+	private Task Log(string msg)
+	{
+		Console.WriteLine(msg);
 		return Task.CompletedTask;
 	}
 
-	
 }
