e728da004a111e50583453daa1c72fa748ebb82a
Chloe Edwards <git@edken.co.uk>
2023-03-25 17:54:38 +0000

    Add project files.

---
+++ RedirectBot/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+public class Program
+{
+	public static Task Main(string[] args)
+	{
+		Program program = new Program();
+		return program.MainAsync();
+	}
+
+	public async Task MainAsync()
+	{
+		//TODO
+	}
+}
