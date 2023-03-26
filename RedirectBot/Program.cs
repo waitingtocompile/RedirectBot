6bd7bcf098394ae4f5d9270b30081b578dfa253d
Chloe Edwards <git@edken.co.uk>
2023-03-26 16:52:59 +0100

    added protected channel behaviour

--- RedirectBot/Program.cs
+++ RedirectBot/Program.cs
@@ -1,10 +1,26 @@
 ﻿using Discord;
+using Discord.Rest;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 
 public class AppConfig
 {
 	public string? token;
+
+	public List<ProtectedChannelGroup>? protectedChannelGroups;
+}
+
+public class ProtectedChannelGroup
+{
+	public ulong roleID { get; private set; }
+	public string moveMessage { get; private set; }
+
+	[JsonConstructor]
+	public ProtectedChannelGroup(ulong roleID, string moveMessage)
+	{
+		this.roleID = roleID;
+		this.moveMessage = moveMessage;
+	}
 }
 
 public class Program
@@ -31,7 +47,9 @@ public class Program
 		}
 	}
 
-	public DiscordSocketClient _client { get; private set; }
+	private DiscordSocketClient _client;
+
+	public Dictionary<ulong, string> roleProtectedMoveMessages { get; private set; }
 
 	Program()
 	{
@@ -39,6 +57,8 @@ public class Program
 		_client.Log += Log;
 		_client.Ready += RefreshCommands;
 		_client.SlashCommandExecuted += ExecuteMoveCommand;
+
+		roleProtectedMoveMessages = new Dictionary<ulong, string>();
 	}
 
 	public async Task RunAsync(AppConfig? config)
@@ -53,20 +73,31 @@ public class Program
 			throw new ArgumentNullException("Discord API key was absent or empy, check the application configuration file");
 		}
 
+		if(config.protectedChannelGroups != null)
+		{
+			foreach(var group in config.protectedChannelGroups)
+			{
+				roleProtectedMoveMessages.Add(group.roleID, group.moveMessage);
+			}
+		}
+
 		await _client.LoginAsync(TokenType.Bot, config.token);
 		await _client.StartAsync();
 
-		//just block for now
+		//our work is done, block and let the client run off in worker threads
 		await Task.Delay(-1);
 	}
 
 	public async Task RefreshCommands()
 	{
-		//this doesn't actually need to be done every time it's run, only when the command is new or changing
-		//I should probably do something about that
+		if((await _client.GetGlobalApplicationCommandsAsync()).Any(command=> command.Name == "move"))
+		{
+			// we have already registered the command, no need to do so again.
+			// if we ever need to add command versioning, this will need to be updated
+			return;
+		}
 
 		var commandBuilder = new SlashCommandBuilder();
-
 
 		commandBuilder.WithName("move")
 			.WithDescription("Send discussion to another channel")
@@ -74,11 +105,12 @@ public class Program
 				ApplicationCommandOptionType.Channel,
 				"The channel to send discussion to",
 				isRequired:true,
-				channelTypes:new List<ChannelType> { ChannelType.Text });
+				channelTypes:new List<ChannelType> { ChannelType.Text })
+			.WithDMPermission(false);
 
 		try
 		{
-			await _client.CreateGlobalApplicationCommandAsync(commandBuilder.Build());
+			var command = await _client.CreateGlobalApplicationCommandAsync(commandBuilder.Build());
 
 			await Log("registered the move slash command");
 		}
@@ -128,14 +160,95 @@ public class Program
 			return;
 		}
 
+		await Log($"performed move from {originChannel.Id} to {targetChannel.Id}, at the request of {command.User.Id}");
+
 		await command.RespondAsync("Moving the discussion now...", ephemeral:true);
 
-		var destinationMessage = await targetChannel.SendMessageAsync($"Continuing the discussion from <#{originChannel.Id}>");
-		var originMessage = await originChannel.SendMessageAsync($"This discussion has moved to <#{targetChannel.Id}> (moved by <@{command.User.Id}>)\n" +
+		var destinationMessage = await targetChannel.SendMessageAsync(embed: ComposeMoveFromMessage(originChannel, null, command.User));
+		var originMessage = await originChannel.SendMessageAsync(embed:ComposeMoveToMessage(targetChannel, destinationMessage, command.User));
+
+		await destinationMessage.ModifyAsync(messageProperties => messageProperties.Embed = ComposeMoveFromMessage(originChannel, originMessage, command.User));
+	}
+
+	public List<string> FindMoveMessages(SocketTextChannel channel)
+	{
+		List<string> messages = new List<string>();
+
+		List<ulong> inheritingIds = new List<ulong>();
+
+		foreach(var permissionOverwrite in channel.PermissionOverwrites)
+		{
+			if (permissionOverwrite.TargetType == PermissionTarget.Role)
+			{
+
+
+				if (permissionOverwrite.Permissions.ViewChannel == PermValue.Allow)
+				{
+					var message = roleProtectedMoveMessages.GetValueOrDefault(permissionOverwrite.TargetId);
+					if (message != null)
+					{
+						messages.Add(message);
+					}
+				}
+				else if (permissionOverwrite.Permissions.ViewChannel == PermValue.Inherit)
+				{
+					inheritingIds.Add(permissionOverwrite.TargetId);
+				}
+			}
+		}
+
+		if(inheritingIds.Count == 0)
+		{
+			return messages;
+		}
+
+		foreach(var permissionOverwrite in channel.Category.PermissionOverwrites)
+		{
+			if(inheritingIds.Contains(permissionOverwrite.TargetId) && permissionOverwrite.Permissions.ViewChannel == PermValue.Allow)
+			{
+				var message = roleProtectedMoveMessages.GetValueOrDefault(permissionOverwrite.TargetId);
+				if (message != null)
+				{
+					messages.Add(message);
+				}
+			}
+		}
+
+		return messages;
+	}
+
+	public Embed ComposeMoveToMessage(SocketTextChannel targetChannel, RestUserMessage destinationMessage, SocketUser movedBy)
+	{
+		EmbedBuilder builder = new EmbedBuilder();
+		builder.WithAuthor(movedBy)
+			.WithColor(Color.Teal)
+			.WithTitle("Discussion Moved")
+			.WithDescription($"Moving the discussion to {targetChannel.Mention}\n" +
 			$"Carry it on at {destinationMessage.GetJumpUrl()}");
 
-		await destinationMessage.ModifyAsync(messageProperties => messageProperties.Content = $"Continuing the discussion from <#{originChannel.Id}>\n" +
-		$"Carried on from {originMessage.GetJumpUrl()}");
+		foreach(var extraMessage in FindMoveMessages(targetChannel))
+		{
+			builder.Description += $"\n\n{extraMessage}";
+		}
+		
+		return builder.Build();
+	}
+
+	public Embed ComposeMoveFromMessage(SocketTextChannel originChannel, RestUserMessage? originMessage, SocketUser movedBy)
+	{
+		EmbedBuilder builder = new EmbedBuilder();
+		builder.WithAuthor(movedBy)
+			.WithColor(Color.Teal)
+			.WithTitle("Discussion Moved Here")
+			.WithDescription($"Continuing the discussion from {originChannel.Mention}\n" +
+			(originMessage==null ? "" : $"See the earlier discussion at {originMessage.GetJumpUrl()}"));
+
+		foreach (var extraMessage in FindMoveMessages(originChannel))
+		{
+			builder.Description += $"\n\n{extraMessage}";
+		}
+
+		return builder.Build();
 	}
 
 	private Task Log(LogMessage msg)
